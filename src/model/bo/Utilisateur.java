c9bc7a9b6b47eba2bff6f72e2e972c58322822de
soukaina <benaddisoukaina@gmail.com>
2018-10-22 21:06:59 +0200

    commit session upload

--- src/model/bo/Utilisateur.java
+++ src/model/bo/Utilisateur.java
@@ -2,64 +2,114 @@ package model.bo;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="USER_TABLE")
+@Table(name = "USER_TABLE")
 public class Utilisateur {
 
-		@Id @GeneratedValue
-		private int id;
-		
-	    private String name;
-	    @Column(unique = true)
-	    private String email;
-	    private String mark;
-	    private String comment_site;
-	    private String password;
-	    
-	    
-		public String getName() {
-			return name;
-		}
-		public void setName(String name) {
-			this.name = name;
-		}
-		public String getMark() {
-			return mark;
-		}
-		public void setMark(String mark) {
-			this.mark = mark;
-		}
-		public String getComment_site() {
-			return comment_site;
-		}
-		public void setComment_site(String comment_site) {
-			this.comment_site = comment_site;
-		}
-		public String getPassword() {
-			return password;
-		}
-		public void setPassword(String password) {
-			this.password = password;
-		}
-		public int getId() {
-			return id;
-		}
-		public void setId(int id) {
-			this.id = id;
-		}
-		
-		public String getEmail() {
-			return email;
-		}
-		public void setEmail(String email) {
-			this.email = email;
-		}
-	    
-	    
+	@Id
+	@GeneratedValue
+	private int id;
+
+	//private String firstname;
+	//private String lastname;
+	private String name;
+	@Column(unique = true)
+	private String email;
+	private String password;
+	private String numberphone;
+	private String about;
+	private String Occupation;
+	private String interest;
 	
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name = "AVATAR_ID", nullable = true)
+	private Avatar avatar;
+	
+	
+	
+	public String getInterest() {
+		return interest;
+	}
+
+	public void setInterest(String interest) {
+		this.interest = interest;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	
+
+	
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+
+	public String getAbout() {
+		return about;
+	}
+
+	public void setAbout(String about) {
+		this.about = about;
+	}
+
+	public String getOccupation() {
+		return Occupation;
+	}
+
+	public void setOccupation(String occupation) {
+		Occupation = occupation;
+	}
+
+	public String getNumberphone() {
+		return numberphone;
+	}
+
+	public void setNumberphone(String numberphone) {
+		this.numberphone = numberphone;
+	}
+
+	public Avatar getAvatar() {
+		return avatar;
+	}
+
+	public void setAvatar(Avatar avatar) {
+		this.avatar = avatar;
+	}
 
 }
--- src/model/dao/UtilisateurDao.java
+++ src/model/dao/UtilisateurDao.java
@@ -1,26 +1,30 @@
 package model.dao;
 
-
-	import org.hibernate.Query;
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.json.JSONObject;
 
 import model.bo.Utilisateur;
 import utils.HibernateUtil;
-	 
-	public class UtilisateurDao {
-	
-	
-	public boolean saveDetails(String name, String email,String password, String mark, String comment_site){
-	{
-	    boolean flag=true;
+
+public class UtilisateurDao {
+
+	public boolean saveDetails(String name, String email, String password, String about, String interest,
+			String occupation, String numberphone, String action) {
+		
+		boolean flag=true;
 	      Session session = HibernateUtil.openSession();
 	      Utilisateur utilisateur=new Utilisateur();
 	        utilisateur.setName(name);
 	        utilisateur.setEmail(email);
-	        utilisateur.setMark(mark);
 	        utilisateur.setPassword(password);
-	        utilisateur.setComment_site(comment_site);
+	        utilisateur.setAbout(about);
+	        utilisateur.setInterest(interest);
+	        utilisateur.setOccupation(occupation);
+	        utilisateur.setNumberphone(numberphone);
+	        
+	        
 	     
 	      Transaction transaction=session.beginTransaction();
 	      try
@@ -36,29 +40,50 @@ import utils.HibernateUtil;
 	      session.close();
 	      return flag;
 	      }
-	}
-	public Utilisateur getUserByUserId(String email, String password) {
-        Session session = HibernateUtil.openSession();
-        Transaction tx = null;
-        Utilisateur user = null;
-        try {
-            tx = session.getTransaction();
-            tx.begin();
-            Query query = session.createQuery("from Utilisateur as user where user.email= :email and user.password= :password");
-            query.setParameter("email", email);
-            query.setParameter("password", password);
-            user = (Utilisateur)query.uniqueResult();
-            tx.commit();
-        } catch (Exception e) {
-            if (tx != null) {
-                tx.rollback();
-            }
-            e.printStackTrace();
-        } finally {
-            session.close();
-        }
-        return user;
-    }
-     
 
+	public Utilisateur getUserByUserId(String email, String password) {
+		Session session = HibernateUtil.openSession();
+		Transaction tx = null;
+		Utilisateur user = null;
+		try {
+			tx = session.getTransaction();
+			tx.begin();
+			Query query = session
+					.createQuery("from Utilisateur as user where user.email= :email and user.password= :password");
+			query.setParameter("email", email);
+			query.setParameter("password", password);
+			user = (Utilisateur) query.uniqueResult();
+			tx.commit();
+		} catch (Exception e) {
+			if (tx != null) {
+				tx.rollback();
+			}
+			e.printStackTrace();
+		} finally {
+			session.close();
+		}
+		return user;
+	}
+
+	public String getNameByUserId(int id) {
+		Session session = HibernateUtil.openSession();
+		Transaction tx = null;
+		Utilisateur user = null;
+		try {
+			tx = session.getTransaction();
+			tx.begin();
+			Query query = session.createQuery("from Utilisateur as user where user.id= :id");
+			query.setParameter("id", id);
+			user = (Utilisateur) query.uniqueResult();
+			tx.commit();
+		} catch (Exception e) {
+			if (tx != null) {
+				tx.rollback();
+			}
+			e.printStackTrace();
+		} finally {
+			session.close();
+		}
+		return user.getName();
+	}
 }
